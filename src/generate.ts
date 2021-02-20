efad30f7da2fb05c1d9d5d60e71460226a641172
Septs <github@septs.pw>
2021-02-20 14:56:05 +0800

    rewrite generate script

--- src/generate.ts
+++ src/generate.ts
@@ -2,40 +2,36 @@ import { randomHex, soliditySha3 } from 'web3-utils'
 import { MerkleTree } from './merkleTree'
 import { buf2hex, hex2buf } from './helpers'
 import fs from 'fs'
-import endent from 'endent'
 
-const rawLeaves = [...Array(3000)].map((v) => ({ address: randomHex(20), amount: randomHex(32) }))
-const leaves = rawLeaves.map((v, i) => {
-  return {
-    buf: Buffer.concat([hex2buf(i.toString(16)), hex2buf(v.address), hex2buf(v.amount)]),
-    ...v,
-  }
-})
+const rawLeaves = [...Array(3000)].map(() => ({
+  address: randomHex(20),
+  amount: randomHex(32),
+}))
 
-export const tree = new MerkleTree(
+const leaves = rawLeaves.map(({ address, amount }, index) => ({
+  buf: Buffer.concat([hex2buf(index.toString(16)), hex2buf(address), hex2buf(amount)]),
+  address,
+  amount,
+}))
+
+const tree = new MerkleTree(
   leaves.map((l) => buf2hex(l.buf)),
   (soliditySha3 as unknown) as (...str: string[]) => string,
 )
 
-export const leavesWithProof = leaves.slice(0, 10).map((l) => {
-  return {
-    address: l.address,
-    proof: tree.generateProof(buf2hex(l.buf)),
-    amount: l.amount,
-  }
-})
+const leavesWithProof = leaves.slice(0, 10).map(({ buf, address, amount }) => ({
+  address,
+  proof: tree.generateProof(buf2hex(buf)),
+  amount,
+}))
 
-const template =
-  endent`
-  const merkleRoot = "${tree.root}"
-  const leaves = ${JSON.stringify(leavesWithProof, null, 2)}
+const constants = JSON.stringify(
+  {
+    merkleRoot: tree.root,
+    leaves: leavesWithProof,
+  },
+  null,
+  2,
+)
 
-  module.exports = {
-    merkleRoot,
-    leaves
-  }
-` +
-  `
-`
-
-fs.writeFile('test/constants.js', template, () => {})
+fs.writeFileSync('test/constants.js', `module.exports = ${constants}`)
