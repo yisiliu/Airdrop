b0878744866a50d1a43ca72a412d976e68923b23
Hancheng Zhou <z308114274@gmail.com>
2021-02-20 16:33:33 +0800

    feat: finish merkle tree calc

--- src/generate.ts
+++ src/generate.ts
@@ -3,11 +3,17 @@ import { MerkleTree } from './merkleTree'
 import { buf2hex, hex2buf } from './helpers'
 import fs from 'fs'
 import endent from 'endent'
+import Web3 from 'web3'
 
-const rawLeaves = [...Array(3000)].map((v) => ({ address: randomHex(20), amount: randomHex(32) }))
+const web3 = new Web3()
+const rawLeaves = [...Array(3000)].map((v) => ({ address: randomHex(20), amount: Math.floor(Math.random() * 100000) }))
 const leaves = rawLeaves.map((v, i) => {
   return {
-    buf: Buffer.concat([hex2buf(i.toString(16)), hex2buf(v.address), hex2buf(v.amount)]),
+    buf: Buffer.concat([
+      hex2buf(web3.eth.abi.encodeParameter('uint256', i)),
+      hex2buf(v.address),
+      hex2buf(web3.eth.abi.encodeParameter('uint256', v.amount)),
+    ]),
     ...v,
   }
 })
