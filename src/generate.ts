787f940fb7ab7f588bec27699c14b3d1cb0066eb
Hancheng Zhou <z308114274@gmail.com>
2021-02-20 14:46:55 +0800

    Update generate.ts

--- src/generate.ts
+++ src/generate.ts
@@ -5,9 +5,9 @@ import fs from 'fs'
 import endent from 'endent'
 
 const rawLeaves = [...Array(3000)].map((v) => ({ address: randomHex(20), amount: randomHex(32) }))
-const leaves = rawLeaves.map((v) => {
+const leaves = rawLeaves.map((v, i) => {
   return {
-    buf: Buffer.concat([hex2buf(v.address), hex2buf(v.amount)]),
+    buf: Buffer.concat([hex2buf(i.toString(16)), hex2buf(v.address), hex2buf(v.amount)]),
     ...v,
   }
 })
